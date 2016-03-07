cda267d3582809f1b959b9651b78e5398592f1d5
unknown <ikazeran@ECE-RTOS26.NEXUS.UWATERLOO.CA>
2016-03-07 01:22:21 -0500

    CONTINUATION OF THE HACK.  WE DONE BOYS.

--- manual_code/Context_Switching_UART/README.txt
+++ manual_code/Context_Switching_UART/README.txt
@@ -7,7 +7,7 @@ Student Number		Student ID		Name
 20440417			ikazeran  	  	Kazerani, Iman   
 20519031			zy9wang  	  	Wang, Zhaoyang 
 
-All source code is in the Context_Switchint_UART/src folder.
-Project root is Context_Switchint_UART.
+All source code is in the Context_Switching_UART/src folder.
+Project root is Context_Switching_UART.
 We included all of the given code as a just in case, not only our project files.
 Documentation.pdf in the project root contains our brief documentation comments at this time.  Will be expanded as we go.
--- manual_code/Context_Switching_UART/src/sys_proc.c
+++ manual_code/Context_Switching_UART/src/sys_proc.c
@@ -22,28 +22,28 @@ PROC_INIT g_sys_procs[NUM_SYS_PROCS];
 void set_sys_procs() {
 	g_sys_procs[0].m_pid=(U32)(0);
 	g_sys_procs[0].m_priority=SYS_NULL_PRIORITY;
-	g_sys_procs[0].m_stack_size=0x100;
+	g_sys_procs[0].m_stack_size=0x200;
  	g_sys_procs[0].mpf_start_pc = &proc_null;
 	
 	// TODO: change the priority to make sense
 	g_sys_procs[1].m_pid=(U32)PID_KCD;
 	g_sys_procs[1].m_priority=SYS_HIGHEST;
-	g_sys_procs[1].m_stack_size=0x100;
+	g_sys_procs[1].m_stack_size=0x200;
  	g_sys_procs[1].mpf_start_pc = &proc_kcd;
 	
 	g_sys_procs[2].m_pid=(U32)PID_CRT;
 	g_sys_procs[2].m_priority=SYS_HIGHEST;
-	g_sys_procs[2].m_stack_size=0x100;
+	g_sys_procs[2].m_stack_size=0x200;
  	g_sys_procs[2].mpf_start_pc = &proc_crt;
 	
 	g_sys_procs[3].m_pid=(U32)PID_TIMER_IPROC;
 	g_sys_procs[3].m_priority=SYS_I_PROC;
-	g_sys_procs[3].m_stack_size=0x100;
+	g_sys_procs[3].m_stack_size=0x200;
  	g_sys_procs[3].mpf_start_pc = &proc_null; // so we know if we messed up
 	
 	g_sys_procs[4].m_pid=(U32)PID_UART_IPROC;
 	g_sys_procs[4].m_priority=SYS_I_PROC;
-	g_sys_procs[4].m_stack_size=0x100;
+	g_sys_procs[4].m_stack_size=0x200;
  	g_sys_procs[4].mpf_start_pc = &proc_null; // so we know if we messed up
 }
 
